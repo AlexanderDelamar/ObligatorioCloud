1bc5ffd5f1c98a4b31fd2955991c0ddc0991141e
AlexanderDelamar <alexanderdelamar1@gmail.com>
2023-06-23 01:25:49 -0300

    Chau bastion

--- bastion.tf
+++ bastion.tf
@@ -21,22 +21,22 @@ resource "null_resource" "imagenes"{
     private_key = file("labsuser.pem")
   }
 
-  provisioner "file" {
-    source = "build-docker-images.sh"
-    destination = "/home/ec2-user/build-docker-images.sh"
-  }
+  # provisioner "file" {
+  #   source = "build-docker-images.sh"
+  #   destination = "/home/ec2-user/build-docker-images.sh"
+  # }
 
-  provisioner "file" {
-    source = "/home/admin/nuevo/ObligatorioCloud/src/adservice/"
-    destination = "/home/ec2-user/src"
-  }
+  # provisioner "file" {
+  #   source = "/home/admin/nuevo/ObligatorioCloud/src/adservice/"
+  #   destination = "/home/ec2-user/src"
+  # }
 
   provisioner "remote-exec" {
     inline = [
       "sudo yum install -y git",
-      "sudo yum install -y docker",
-      "sudo chmod +x /home/ec2-user/build-docker-images.sh",
-      "sudo sh /home/ec2-user/build-docker-images.sh"
+      "sudo yum install -y docker"
+      # "sudo chmod +x /home/ec2-user/build-docker-images.sh",
+      # "sudo sh /home/ec2-user/build-docker-images.sh"
     ]
   }
 
